727f875482a37071d5e2fcfb9b040fe56181eb53
Hannah <reachhannahs@gmail.com>
2022-07-07 09:48:22 -0700

    user input to calculate area

---
+++ Unit 1/OnepointFour.py
@@ -0,0 +1,5 @@
+# A program that calculates area
+width = int(input("Enter with width: "))
+height = int(input("Enter with height: "))
+area = width * height
+print("The area is",area,"square units.")
