25a5a25a66a8a375cda11e09b27431c5579015b7
Hannah <reachhannahs@gmail.com>
2022-07-09 17:08:18 -0700

    surface area calculated

---
+++ Unit 2/TwopointTwo.py
@@ -0,0 +1,4 @@
+# A program that calculates surface area of a cube
+area_of_a_side = int(input("Enter the cube's edge: "))
+surface_area= 6*(area_of_a_side*area_of_a_side)
+print("The surface area is ", surface_area, " square units.")
