06595a0561a98b2cc2891b3c21dc90eb2cde8e8d
Hannah <reachhannahs@gmail.com>
2022-07-09 17:05:39 -0700

    using the round function

---
+++ Unit 2/TwopointOne.py
@@ -0,0 +1,14 @@
+# A tax calculator
+
+TAX_RATE = 0.20
+STANDARD_DEDUCTION = 10000.0
+DEPENDENT_DEDUCTION = 3000.0
+
+grossIncome = float(input("Enter the gross income: "))
+numDependents = int(input("Enter the number of dependents: "))
+
+taxableIncome = grossIncome - STANDARD_DEDUCTION - \
+                DEPENDENT_DEDUCTION * numDependents
+incomeTax = taxableIncome * TAX_RATE
+
+print("The income tax is $" + str(round(incomeTax,2)))
