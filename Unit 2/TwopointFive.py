5c8abdb15ef01d57b7639d38c6413653d00e9cc1
Hannah <reachhannahs@gmail.com>
2022-07-11 14:14:54 -0700

    printing the object's momentum

---
+++ Unit 2/TwopointFive.py
@@ -0,0 +1,4 @@
+# A program that outputs the momentum of an object by taking in mass and velocity as inputs
+mass = float(input("Mass: "))
+velocity = float(input("Velocity: "))
+print("The object's momentum is ", mass*velocity)
