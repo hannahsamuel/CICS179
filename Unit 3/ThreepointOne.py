74db8e75c8e0f51ffdb32436e22086331be3a5c2
Hannah <reachhannahs@gmail.com>
2022-07-11 14:18:01 -0700

    checking the triangle

---
+++ Unit 3/ThreepointOne.py
@@ -0,0 +1,9 @@
+# A program that indicates if a triangle is an equilateral
+first = float(input("Enter the first side: "))
+second = float(input("Enter the second side: "))
+third = float(input("Enter the third side: "))
+if first == second :
+    if second == first:
+        print("The triangle is equilateral.")
+else:
+    print("The triangle is not equilateral.")
