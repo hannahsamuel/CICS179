a8582f7632c6631a935ae08978b9d322295cd786
Hannah <reachhannahs@gmail.com>
2022-07-20 09:23:15 -0700

    calculate surface area

--- Unit 2/cube.py
+++ Unit 2/cube.py
@@ -1,4 +1,4 @@
-# A program that calculates surface area of a cube
+# A program that calculates surface area of a cube in square unites
 area_of_a_side = int(input("Enter the cube's edge: "))
-surface_area= 6*(area_of_a_side*area_of_a_side)
+surface_area = 6*(area_of_a_side*area_of_a_side)
 print("The surface area is ", surface_area, " square units.")
